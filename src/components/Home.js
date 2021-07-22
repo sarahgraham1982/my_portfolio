91077c5ae82c304e02825ca7b7751cc3772c8a71
sarahgraham1982 <79756187+sarahgraham1982@users.noreply.github.com>
2021-07-22 21:30:49 +0100

    random commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,18 +4,25 @@ import '../components/Home.css';
 
 const Home = () => {
 	return ( 
-        <div className="container">
-          <div className="welcome-text">
-            <h1>Hi, I'm Sarah Graham</h1>
-            <h5>and this is Oscar, but he doesn't code...yet!</h5>
-          </div>
-          <div className="button">
-            <a href ="/projects">View Projects</a>
-          </div>           
-          <div className="image">
-            <img src={sarah} alt="Sarah and Oscar" width="500" height="500" />
-          </div>         
+    <div className="welcome-container">
+         
+      <div className="left">
+        <div className="welcome-text">
+          <h1>Hi, I'm Sarah!</h1>
+          <h5>and this is Oscar, but he doesn't code...yet!</h5>
         </div>
+
+        <div className="button">
+          <a href ="/projects">View Projects</a>
+        </div> 
+      </div> 
+
+      <div className="right">
+        <div className="image">
+          <img src={sarah} alt="Sarah and Oscar" width="500" height="500" />
+        </div> 
+      </div>           
+    </div>
     );
   };
 
