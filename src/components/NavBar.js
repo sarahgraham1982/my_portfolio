5b8e96968666540fef1f4d194f85c7f1b1a5fbec
sarahgraham1982 <79756187+sarahgraham1982@users.noreply.github.com>
2021-07-23 18:57:01 +0100

    more nav commits

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,35 +9,37 @@ const NavBar = () => {
 
   return (
   <>
-    <div className="NavBar">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About Me</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-      </ul>
-    </div>
-    <div className="SocialBar">
-      <ul>
-        <li>
-          <a href="https://www.linkedin.com/in/sarahvgraham/">
-          <img alt="LinkedIn" src="/LinkedIn.png"></img></a>
-        </li>
-        <li>
-        <a href="https://github.com/sarahgraham1982">
-          <img alt="GitHub" src="/GitHub.png"></img></a>
-        </li>
-        <li>
-        <a href="https://www.instagram.com/sarahinlalaland">
-          <img alt="Instagram" src="/Instagram.png"></img></a>
-        </li>
-      </ul>
-    </div>
+    <div className="Nav-Container">
+      <div className="NavBar">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Me</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="SocialBar">
+        <ul>
+          <li>
+            <a href="https://www.linkedin.com/in/sarahvgraham/">
+            <img alt="LinkedIn" src="/LinkedIn.png"></img></a>
+          </li>
+          <li>
+          <a href="https://github.com/sarahgraham1982">
+            <img alt="GitHub" src="/GitHub.png"></img></a>
+          </li>
+          <li>
+          <a href="https://www.instagram.com/sarahinlalaland">
+            <img alt="Instagram" src="/Instagram.png"></img></a>
+          </li>
+        </ul>
+      </div>
+    </div>  
   </>
   );
 }
