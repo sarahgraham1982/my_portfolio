cf3c3f51e2f9e79d22063efa339db1c5791621f5
sarahgraham1982 <79756187+sarahgraham1982@users.noreply.github.com>
2021-07-24 18:02:04 +0100

    added about page info

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,15 @@ import '../components/About.css';
 const About = () => {
   return (
     <div className="About">
-      <h4>About Me.</h4>
+      <h1>About Me.</h1>
+      <h3>"Probably the best Software Developer I know" - my mum</h3>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Before I qualified as a Software Developer, I ran my own successful online store called <a href="https://www.fangrrrl.com/">Fan Grrrl</a> for over a decade. Using my graphic design skills, I created all the pop culture inspired products and performed every aspect of shop management myself. Having used most of the popular E-commerce platforms over the years, I felt the next logical step in my career was to learn how to create websites from scratch, instead of relying on pre-made templates that often were limiting in terms of functionality. After extensive research, I found <a href="https://codeclan.com/">Code Clan</a> and completed their 16 week intensive <a href="https://codeclan.com/courses/professional-software-development/">Professional Software Development</a> course. It was a huge challenge and I enjoyed it immensely. I believe that I am now in the unique position of having both coding and graphic design skills.
       </p>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        In my spare time, I enjoy going to see 90s indie bands and obscure stand-up comedy, decorating my new home and going for long walks with my dog. 
       </p>
+      
     </div>
   );
 };
