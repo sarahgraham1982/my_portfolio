bb361dd12a908040224d4ff3cd243516a7e46396
sarahgraham1982 <79756187+sarahgraham1982@users.noreply.github.com>
2021-07-24 23:54:59 +0100

    final commit tonight

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,9 +7,9 @@ const Projects = () => {
   <div className="Projects">
       <h1>Projects.</h1>
       <h3>"I enjoy being creative with my projects and love to see my code work!"</h3>
-      <ul>
-        <li><strong>BAND TEASE</strong></li>
-        </ul>
+      
+        <h4><strong>BAND ϟ TEASE</strong></h4>
+        
         <iframe width="560" height="315" src="https://www.youtube.com/embed/jSokYTC4NYQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
       
