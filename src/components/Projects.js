f9a4132f5dbaf64bc783fe821913bcb5d309d9f6
sarahgraham1982 <79756187+sarahgraham1982@users.noreply.github.com>
2021-07-24 20:45:49 +0100

    added youtube vide to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,8 +9,12 @@ const Projects = () => {
       <h3>"I enjoy being creative with my projects and love to see my code work!"</h3>
       <ul>
         <li><strong>BAND TEASE</strong></li>
-        A solo Python project, completed in 7 days, utilizing Flask, PostgreSQL, OOP, TDD, RESTful routes, HTML and CSS. This project was fun and I especially enjoyed working with the database. <a href="https://github.com/sarahgraham1982/band_tease">Click to view the repo on GitHub</a>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/jSokYTC4NYQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
       </ul>
+        A solo Python project, completed in 7 days, utilizing Flask, PostgreSQL, OOP, TDD, RESTful routes, HTML and CSS. This project was fun and I especially enjoyed working with the database. <a href="https://github.com/sarahgraham1982/band_tease">Click to view the repo on GitHub</a>
+      {/* </ul> */}
     </div>
   );
 };
